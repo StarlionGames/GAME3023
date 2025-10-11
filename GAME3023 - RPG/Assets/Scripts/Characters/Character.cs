f87e3093ef7c10d4a63b9ed6fb8f3c3bb1d2f82a
StarlionGames <starliongames.dev@gmail.com>
2025-10-11 15:48:51 -0400

    Update Character.cs

--- GAME3023 - RPG/Assets/Scripts/Characters/Character.cs
+++ GAME3023 - RPG/Assets/Scripts/Characters/Character.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-[System.Serializable]
 [CreateAssetMenu(menuName = "Character/New Base Character")]
 public class Character: ScriptableObject
 {
